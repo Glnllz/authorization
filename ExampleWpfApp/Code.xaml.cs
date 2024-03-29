52ccabbe13fcb7cc33afad94d37527117e822c70
Glnllz <lizagalina4@gmail.com>
2024-03-30 02:47:22 +0500

    готова

---
+++ ExampleWpfApp/Code.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExampleWpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Code.xaml
+    /// </summary>
+    public partial class Code : Window
+    {
+        public static string sendedCode;
+        public Code()
+        {
+            InitializeComponent();
+        }
+        private void AcceptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (sendedCode == CodeTB.Text)
+            {
+                Console.WriteLine("Все хорошо!!");
+                MessageBox.Show("Вход успешен");
+            }
+            else
+            {
+                MessageBox.Show("Вы не ввели код или он неправильный");
+            }
+        }
+    }
+}
--- ExampleWpfApp/MainWindow.xaml.cs
+++ ExampleWpfApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static ExampleWpfApp.Code;
+
+
 
 namespace ExampleWpfApp
 {
@@ -21,37 +26,76 @@ namespace ExampleWpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string login = "admin";
-        private string password = "admin";
         public MainWindow()
         {
             InitializeComponent();
         }
-        List<string> contents = new List<string>() { "Милкризу", "Лизи пизи смоук"};
-        List<Brush> colors = new List<Brush>() { Brushes.HotPink, Brushes.Blue };
-        int indexPosition = 0;
-        private void SimpleBtn_Click(object sender, RoutedEventArgs e)
-        {
-            indexPosition++;
-            SimpleBtn.Background = colors[indexPosition % colors.Count()];
-            SimpleBtn.Content = contents[indexPosition % contents.Count()];
 
-            if (password != "171519")
+        private void SignInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmailTB.Text != string.Empty || PasswordTB.Password != string.Empty)
             {
-                MessageBox.Show("Вы успешно авторизовались!");
+                Code codeWindow = new Code();
+                codeWindow.Show();
+
+                MailSender.SendCode("ilizagalina4@gmail.com");
             }
             else
             {
-
+                MessageBox.Show("Вы забыли ввести пароль или логин!");
             }
-            
         }
-
-        private void SimpleTB_TextChanged(object sender, TextChangedEventArgs e)
+        public static class MailSender
         {
-            SimpleLb.Content = SimpleTB.Text;
+            public static void SendMail(string recipient, string subject, string body)
+            {
+                // кто отправляет
+                MailAddress fromM = new MailAddress("ilizagalina4@gmail.com", "C#");
+                // куда отправляет
+                MailAddress toM = new MailAddress(recipient);
+
+                using (MailMessage message = new MailMessage(fromM, toM))
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    message.Subject = subject; // Заголовок
+                    message.Body = body; // Содержание письма
+
+                    smtpClient.Host = "smtp.gmail.com";
+                    smtpClient.Port = 587;
+                    smtpClient.EnableSsl = true;
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.UseDefaultCredentials = false;
+                    // задаем ящик который отправляет письмо (адрес и пароль)
+                    smtpClient.Credentials = new NetworkCredential(fromM.Address, "pfyx oajj ztyk dzov");
+
+                    smtpClient.Send(message);
+                }
+            }
+            public static void SendCode(string recipient)
+            {
+                Random rnd = new Random();
+                int code = rnd.Next(1000, 9999);
+                sendedCode = $"{code}";
+                MailAddress fromM = new MailAddress("ilizagalina4@gmail.com", "C#");
+                MailAddress toM = new MailAddress(recipient); //куда отправить
+
+                using (MailMessage message = new MailMessage(fromM, toM))
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    message.Subject = "Welcome to prop.online!"; // Заголовок
+                    message.Body = $"<h1>This is code for you registration : {code}</h1>"; //Содержание
+                    message.IsBodyHtml = true;
+
+                    smtpClient.Host = "smtp.gmail.com";
+                    smtpClient.Port = 587;
+                    smtpClient.EnableSsl = true;
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = new NetworkCredential(fromM.Address, "pfyx oajj ztyk dzov");
+
+                    smtpClient.Send(message);
+                }
+            }
         }
     }
-
-
 }
